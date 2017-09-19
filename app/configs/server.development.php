eb782c0b5a02bfb694ec63d279f8f96499d7d002
Marcello Veloso <mveloso@fingerprint.com.br>
2017-09-19 13:53:11 -0300

    Senha do banco removida

--- app/configs/server.development.php
+++ app/configs/server.development.php
@@ -16,7 +16,7 @@ return [
         'adapter' => 'Mysql',
         'host' => '127.0.0.1',
         'username' => 'root',
-        'password' => ' ',
+        'password' => '',
         'dbname' => 'phalconApi',
     ],
     'cors' => [
